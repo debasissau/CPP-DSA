f3a1f37d0eef72f9385bd37205dba91f4aaeff94
Debasis Sau <debasissau2004@gmail.com>
2026-08-05 09:45:21 +0530

    Time: 3 ms (66.24%), Space: 14.8 MB (57.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,7 +4,9 @@ public:
         unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++){
             int comp=target-nums[i];
-            if(mp.count(comp))return {i,mp[comp]};
+            if(mp.count(comp)){
+                return {i,mp[comp]};
+            }
             mp[nums[i]]=i;
         }
         return {-1,-1};
