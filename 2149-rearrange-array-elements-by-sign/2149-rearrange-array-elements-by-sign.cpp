1cd676fdc4ed82be4005a839fcd95acc18dba8a3
Debasis Sau <debasissau2004@gmail.com>
2026-08-15 03:37:50 +0530

    Time: 0 ms (100%), Space: 128.2 MB (78.03%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(n);
+        int p=0,ne=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                ans[p]=nums[i];
+                p=p+2;
+            }
+            else{
+                ans[ne]=nums[i];
+                ne=ne+2;
+            }
+        }
+        return ans;
+    }
+};
