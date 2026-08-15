ce8aac50aea3fea0a691abb71cbf2fa3cf914c5e
Debasis Sau <debasissau2004@gmail.com>
2026-08-16 01:31:47 +0530

    Time: 3 ms (29.34%), Space: 8.4 MB (84.07%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,8 +5,8 @@ public:
         while(x!=0){
             int rem=x%10;
             x=x/10;
-            if(ans >INT_MAX/10 || (ans==INT_MAX/10 && rem==7 ))return 0;
-            if(ans <INT_MIN/10 || (ans==INT_MIN/10 && rem==8 ))return 0;
+            if(ans>INT_MAX/10 || (ans==INT_MAX/10 && rem==7))return 0;
+            if(ans<INT_MIN/10 || (ans==INT_MIN/10 && rem==8))return 0;
             ans=ans*10+rem;
         }
         return ans;
