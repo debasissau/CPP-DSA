a28c5e2091be2999b2390c45d4d0c5079ca99512
Debasis Sau <debasissau2004@gmail.com>
2026-08-16 01:40:15 +0530

    Time: 0 ms (100%), Space: 8.6 MB (65.18%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x<0)return false;
-        long long rev=0;
         int ori=x;
+        long long rev=0;
         while(x!=0){
-            int rem=x%10;
+            rev=rev*10+(x%10);
             x=x/10;
-            rev=rev*10+rem;
         }
-        return ori==rev;
+        return rev==ori;
     }
 };
