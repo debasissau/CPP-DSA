3fbdc181b74f947cd0ec01658d1b74e1e9917fa7
Debasis Sau <debasissau2004@gmail.com>
2026-08-17 12:12:45 +0530

    Time: 2 ms (21.33%), Space: 71.8 MB (53.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,36 +1,13 @@
-// class Solution {
-// public:
-//     int maxSubArray(vector<int>& nums) {
-//         long long maxi=INT_MIN;
-//         long long sum=0;
-//         for(int i=0;i<nums.size();i++){
-//             sum+=nums[i];
-//             if(sum>maxi)maxi=sum;
-//             if(sum<0)sum=0;
-//         }
-//         return maxi;
-//     }
-// };
-
-
-
-// if you have to find the maximum_sum's starting and ending index 
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        long long maxi=INT_MIN;
         long long sum=0;
-        int st=-1,en=-1;
+        long long maxi=INT_MIN;
         for(int i=0;i<nums.size();i++){
-            if(sum==0)st=i;
             sum+=nums[i];
-            if(sum>maxi){
-                maxi=sum;
-                en=i;
-            }
+            if(sum>maxi)maxi=sum;
             if(sum<0)sum=0;
         }
-        cout<<st<<" "<<en;
         return maxi;
     }
-};    
+};
